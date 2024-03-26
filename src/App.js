221f3613ec4fc4ddcaf5abed585d7535a590f9a6
panda-codes <dedeomatse@gmail.com>
2024-03-26 22:37:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My User List</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
